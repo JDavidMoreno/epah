925a754e447c6abdafcb3d040198e77c236a9845
JDavidMoreno <david.moreno0440@gmail.com>
2022-10-09 17:00:21 +0200

    Adding an image picker

---
+++ App/Model/ImagePicker.swift
@@ -0,0 +1,39 @@
+//
+//  ImagePicker.swift
+//  Epah
+//
+//  Created by David Moreno on 9/10/22.
+//
+
+import SwiftUI
+import PhotosUI
+
+@MainActor
+class ImagePicker: ObservableObject {
+    @Published var image: Image?
+    @Published var imageSelection: PhotosPickerItem? {
+        didSet {
+            if let imageSelection {
+                Task {
+                    try await loadTransferable(from: imageSelection)
+                }
+            }
+        }
+    }
+    
+    func loadTransferable(from imageSelection: PhotosPickerItem?) async throws {
+        do {
+            if let data = try await imageSelection?.loadTransferable(type: Data.self) {
+                if let uiImage = UIImage(data: data) {
+                    self.image = Image(uiImage: uiImage)
+                }
+            }
+        } catch {
+            print(error.localizedDescription)
+            image = nil
+        }
+    }
+    
+    
+}
+
--- App/Producer/ProducerEditor.swift
+++ App/Producer/ProducerEditor.swift
@@ -4,10 +4,13 @@
 //
 //  Created by David Moreno on 5/10/22.
 //
-
+import AVFoundation
 import SwiftUI
+import PhotosUI
 
 struct ProducerEditView: View {
+    
+    @State var image: Image?
     @State var name = ""
     @State var email = ""
     @State var phone = ""
@@ -15,13 +18,37 @@ struct ProducerEditView: View {
     @State var about = ""
     
     @EnvironmentObject private var model: Model
+    @StateObject var imagePicker = ImagePicker()
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
         ZStack {
             Color("Background")
                 .ignoresSafeArea()
+        
             Form {
+                Section {
+                    HStack {
+                        Spacer()
+                        ProfileImage(image: imagePicker.image)
+                            .frame(width: 80, height: 80)
+                            .background(Color.white)
+                            .clipShape(Circle())
+                            .shadow(radius: 3)
+                            .overlay(alignment: .bottomTrailing) {
+                                PhotosPicker(selection: $imagePicker.imageSelection,
+                                             matching: .images,
+                                             photoLibrary: .shared()) {
+                                    Image(systemName: "pencil.circle.fill")
+                                        .symbolRenderingMode(.multicolor)
+                                        .font(.system(size: 30))
+                                }
+                                .buttonStyle(.borderless)
+                            }
+                        Spacer()
+                    }
+                }
+               
                 Section(header: Text("Basic Information")) {
                     TextField("Producer name", text: $name)
                     TextField("Email", text: $email)
@@ -48,6 +75,22 @@ struct ProducerEditView: View {
                     }
                 }
             }
+        }
+    }
+}
+
+struct ProfileImage: View {
+    let image: Image?
+    
+    var body: some View {
+        
+        if let image = image {
+            image
+                .resizable()
+        } else {
+            Image(systemName: "photo")
+                .font(.system(size: 50))
+                
         }
     }
 }
